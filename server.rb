308cbd8a88a5cd46a9efa752906fdbca41f2cc3e
Katie Blair <katieblair89@gmail.com>
2014-06-14 16:21:00 -0400

    Set up files and started writing game

---
+++ server.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'shotgun'
+
+
+get '/index' do
+
+
+end
+
+get '/intro' do
+
+  erb :'intro.html'
+end
+
+get '/lake' do
+
+  erb :'lake.html'
+end
+
+get '/followtrail' do
+
+  erb :'followtrail.html'
+end
+
+get '/drinkfromlake' do
+
+  erb :'drinkfromlake.html'
+end
+
+get '/strangedreamlake' do
+
+  erb :'strangedreamlake.html'
+end
+
+get '/drinkthewine' do
+
+  erb :'drinkthewine.html'
+end
+
+get '/7' do
+
+  erb :'adventure7.html'
+end
+
+get '/8' do
+
+  erb :'adventure8.html'
+end
+
+get '/9' do
+
+  erb :'adventure9.html'
+end
+
+get '/10' do
+
+  erb :'adventure10.html'
+end
+
+
+
+
+
